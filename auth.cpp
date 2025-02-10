f6c556f245c8ccf6452882ac76ae70e9ee612b91
Rene <rener2104@gmail.com>
2025-02-10 16:58:03 -0600

    Add authenticateUser function, which asks user to input a username & password, then checks to see if they match

--- auth.cpp
+++ auth.cpp
@@ -1,1 +1,25 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+void authenticateUser(){
+    const string USER = "admin";
+    const string PASS = "password";
+    bool authenticated = false;
+    int cont = 1;
+
+    while(!authenticated){
+        string username, password;
+        cout << "Enter username:";
+        cin >> username;
+        cout << "Enter password:";
+        cin >> password;
+
+        if (username == USER && password == PASS){
+          authenticated = true;
+          cout << "Welcome!\n";
+        } else {cout << "Authentication failed.\n\n";}
+    }
+}
 
