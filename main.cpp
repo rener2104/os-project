fc57465031662c765d4ec930c149d4269a7e2569
Rene <rener2104@gmail.com>
2025-02-10 14:24:52 -0600

    Create main & os booting up message

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+  cout << "OS is booting up..." << endl;
+  sleep(1);
+  cout << "..." << endl;
+  sleep(1);
+  cout << "..." << endl;
+
+
+
+
+}
