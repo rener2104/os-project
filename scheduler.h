12564af6e14453ae004f01939799e150701f0545
Rene <rener2104@gmail.com>
2025-03-17 22:10:36 -0500

    Create header file for Scheduler class

--- scheduler.h
+++ scheduler.h
@@ -1,4 +1,22 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
+#include <vector>
+#include "Process.h"
+
+class Scheduler {
+
+  public:
+    void FCFS(std::vector<Process>& processes);
+    void SJF(std::vector<Process>& processes, bool preemptive = false);
+    void PrintResults();
+
+  private:
+    std::vector<int> completionTimes;
+    std::vector<int> turnaroundTimes;
+    std::vector<int> waitingTimes;
+    double averageTurnaroundTime = 0.0;
+    double averageWaitingTime = 0.0;
+};
+
 #endif //SCHEDULER_H
